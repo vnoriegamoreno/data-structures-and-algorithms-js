2d07b5f6886358adf6046c1d20b87ddde29fc367
Vicente Spencer Noriega Moreno <13081953+vnoriegamoreno@users.noreply.github.com>
2021-10-12 15:27:08 -0500

    Initial commit

---
+++ src/data-structures/linked-list/LinkedList.js
@@ -0,0 +1,41 @@
+const Node = require("../node/Node");
+
+module.exports = class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = newNode;
+    this.length = 1;
+  }
+
+  push(value) {
+    /**
+     * Big O(n)
+     * Algorithm steps
+     *  1.- Create a new Node
+     *  2.- add Node to the end of the list
+     */
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    let temp = this.head;
+    let pre = this.head;
+    while (temp.next) {
+      pre = temp;
+      temp = temp.next;
+    }
+  }
+};
